7f5dc60d330cad957b7432f0eb2ea05851040c67
Charanjeet Singh <charanjeetshaktawat@gmail.com>
2020-09-10 11:13:45 +0530

    Added : Loop through Multiple times Simulator

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,6 +1,14 @@
 echo "Welcome to Flip Coin Simulation Program"
-random=$(( RANDOM%2 ))
-if [ $random -eq 1 ]
-then echo "Head wins"
-else echo "Tail wins"
-fi
+read -p "Enter no of times for the loop to be executed " n
+echo "Random no when Head wins=1"
+echo "Random no when Tail Wins=0"
+for ((i=1;i<=n;i++))
+do
+	random=$(( RANDOM%2 ))
+	if [ $random -eq 1 ]
+	then ((h++))
+	else ((t++))
+	fi
+done
+echo "Head wins : $h times"
+echo "Tail wins : $t times"
